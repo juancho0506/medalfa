cae4b263d6a393e9269128972901c1aa182dc073
Rodrigo Torres <juanro.toes@gmail.com>
2013-02-26 00:05:21 -0500

    Corregidas las entidades para crear el schema

--- desarrollo/MedAlpha/src/Dinamica/MedAlfa/MedAlfaBundle/Entity/Medico.php
+++ desarrollo/MedAlpha/src/Dinamica/MedAlfa/MedAlfaBundle/Entity/Medico.php
@@ -117,8 +117,8 @@ class Medico {
 	 * @var EspecialidadMD $especialidadMD
 	 *  @ORM\ManyToMany(targetEntity="EspecialidadMD", inversedBy="medicos")
 	 *  @ORM\JoinTable(name="medico_especialidad",
-	 *      joinColumns={@JoinColumn(name="especialidad_id", referencedColumnName="id")},
-	 *      inverseJoinColumns={@JoinColumn(name="medico_id", referencedColumnName="id")}
+	 *      joinColumns={@ORM\JoinColumn(name="especialidad_id", referencedColumnName="id")},
+	 *      inverseJoinColumns={@ORM\JoinColumn(name="medico_id", referencedColumnName="id")}
 	 *      )
 	 */
 	private $especialidadesMD;
